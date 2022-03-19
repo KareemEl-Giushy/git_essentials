d2f19b77026fb5bb1627c2d6a8c69f2b1e13fa1c
Sabry <icavalor@gmail.com>
2022-03-19 13:40:37 +0200

    added list-comp.py

---
+++ list_comprehensions.py
@@ -0,0 +1,15 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+
+
+def remove_hpp(filename: str):
+    if filename.endswith(".hpp"):
+        filename = filename[:-3] + "h"
+    return filename
+
+
+newfilenames = [remove_hpp(filename) for filename in filenames]
+
+print(newfilenames)
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
